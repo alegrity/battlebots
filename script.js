9ecbb1346377a225aa21295a738fa898a67ea15e
BenjaminJL25 <142451856+BenjaminJL25@users.noreply.github.com>
2024-09-07 14:54:19 -0700

    init

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function sendGetRequest(url, data) {
-    // Construct URL with parameters
     const params = new URLSearchParams(data).toString();
     const fullUrl = `${url}?${params}`;
 
@@ -21,17 +20,15 @@ const joystickContainer = document.getElementById('joystick-container');
 let containerRect = joystickContainer.getBoundingClientRect();
 let lastSendTime = 0;
 
-function updateVectorPosition(clientX, clientY) {
-    let offsetX = clientX - containerRect.left;
-    let offsetY = clientY - containerRect.top;
+function updateVectorPosition(touch) {
+    let offsetX = touch.clientX - containerRect.left;
+    let offsetY = touch.clientY - containerRect.top;
 
     let x = (offsetX / containerRect.width) * 2 - 1;
     let y = (offsetY / containerRect.height) * 2 - 1;
 
-    if (x > 1) x = 1;
-    if (x < -1) x = -1;
-    if (y > 1) y = 1;
-    if (y < -1) y = -1;
+    x = Math.max(-1, Math.min(1, x));
+    y = Math.max(-1, Math.min(1, y));
 
     let angle = Math.atan2(y, x) * (180 / Math.PI);
     let length = Math.sqrt(x * x + y * y);
@@ -50,24 +47,26 @@ function resetVectorPosition() {
     sendGetRequest('/vector', { x: 0, y: 0 });
 }
 
-joystickContainer.addEventListener('touchstart', (e) => {
-    updateVectorPosition(e.touches[0].clientX, e.touches[0].clientY);
-});
+function handleTouchEvent(event) {
+    for (let i = 0; i < event.touches.length; i++) {
+        const touch = event.touches[i];
+        if (touch.target.id === 'joystick-container') {
+            updateVectorPosition(touch);
+        }
+    }
+}
 
-joystickContainer.addEventListener('touchmove', (e) => {
-    e.preventDefault(); 
-    updateVectorPosition(e.touches[0].clientX, e.touches[0].clientY);
-});
-
+joystickContainer.addEventListener('touchstart', handleTouchEvent);
+joystickContainer.addEventListener('touchmove', handleTouchEvent);
 joystickContainer.addEventListener('touchend', resetVectorPosition);
 joystickContainer.addEventListener('mousedown', (e) => {
-    updateVectorPosition(e.clientX, e.clientY);
+    updateVectorPosition(e);
     window.addEventListener('mousemove', onMouseMove);
     window.addEventListener('mouseup', onMouseUp);
 });
 
 function onMouseMove(e) {
-    updateVectorPosition(e.clientX, e.clientY);
+    updateVectorPosition(e);
 }
 
 function onMouseUp() {
